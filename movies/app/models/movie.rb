c0b3ca29bff1fd7ce1dbc50573345160bd89d2dc
marklc44 <marklc44@gmail.com>
2013-11-22 11:21:41 -0800

    Removed validations from movie app

--- movies/app/models/movie.rb
+++ movies/app/models/movie.rb
@@ -1,11 +1,11 @@
 class Movie < ActiveRecord::Base
-	validates :title, :description, presence:true
-	validates :description, length: { minimum: 10 } 
-	validates :year_released, numericality: { only_integer: true }                   
+	# validates :title, :description, presence:true
+	# validates :description, length: { minimum: 10 } 
+	# validates :year_released, numericality: { only_integer: true }                   
 	# validates :rating, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5, only_integer: true }
 	
 	def self.search_for(query)
-		Movie.where("title LIKE :query OR description LIKE :query OR year_released = #{query}", :query => "%#{query}%")
+		Movie.where("title LIKE :query OR description LIKE :query", :query => "%#{query}%")
 	end
 
 	before_save :fill_poster
